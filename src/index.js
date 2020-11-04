c3aca0710af4a872c0ae66f6dce75b2a0d100464
giapthutrang <giapthutrang2891@gmail.com>
2020-11-04 22:21:53 +0700

    .

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const app = require ();
 const bodyParser = require(bodyParser);
 
+const Router = require('./routers/class')
+
+app.use('/api/class',Router)
+
 //listen
 const PORT = env.API_PORT;
 app.listen(PORT, (err) => {
