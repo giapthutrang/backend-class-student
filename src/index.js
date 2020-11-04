1fad5daa19dcaa265d1c180520bb78dfb65bfc8e
giapthutrang <giapthutrang2891@gmail.com>
2020-11-04 22:32:19 +0700

    .

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ const cors = require('cors');
 const app = require ();
 const bodyParser = require(bodyParser);
 
-const Router = require('./routers/class')
+app.use(bodyParser.json())
 
+const Router = require('./routers/class')
 app.use('/api/class',Router)
+
+const log = (req, res, next) => {
+  console.log('==========================')
+  console.log('req.originalUrl\t', req.originalUrl);
+  console.log('req.query\t', req.query);
+  console.log('req.params\t', req.params);
+  console.log('req.body\t', req.body); // chi post moi co 
+  next();
+}
+app.use(log);
 
 //listen
 const PORT = env.API_PORT;
